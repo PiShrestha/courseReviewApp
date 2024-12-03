11e0eef0b0cca7c67428ad14b7383c8c638c59b1
kyle-luong <133829822+kyle-luong@users.noreply.github.com>
2024-12-03 18:44:01 -0500

    CourseReviewApplication partial implementation

--- src/main/java/edu/virginia/sde/reviews/CourseReviewsApplication.java
+++ src/main/java/edu/virginia/sde/reviews/CourseReviewsApplication.java
@@ -5,17 +5,42 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.SQLException;
+
 public class CourseReviewsApplication extends Application {
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-world.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello World");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        var databaseConnection = new DatabaseConnection("appDatabase.db");
+        databaseConnection.connect();
+
+        try {
+            databaseConnection.connect();
+            databaseConnection.createTables();
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while building the database", e);
+        }
+
+        var userDatabase = new UserDatabase(databaseConnection);
+        var courseDatabase = new CourseDatabase(databaseConnection);
+        var reviewDatabase = new ReviewDatabase(databaseConnection);
+
+        var userService = new UserService(userDatabase);
+        var courseService = new CourseService(courseDatabase);
+        var reviewService = new ReviewService(reviewDatabase);
+
+        var fxmlLoader = new FXMLLoader(getClass().getResource("login.fxml"));
+        var scene = new Scene(fxmlLoader.load());
+
+        var controller = (LoginController) fxmlLoader.getController();
+        controller.setPrimaryStage(primaryStage);
+        controller.setServices(userService, courseService, reviewService);
+
+        primaryStage.setTitle("Course Reviews");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
